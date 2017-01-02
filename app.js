2c1f19a2cd9a346f0986c1438506272d48bad284
Thomas <thomas.rundle@conduitlearning.com>
2017-01-02 11:07:34 -0600

    Added another html page as an example

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const publicDir = path.join(__dirname, 'public');
 
+app.use(express.static(publicDir));
+app.get('/contact', (req, res) => {
+    res.sendFile( path.join(publicDir, 'contact.html' ));
+});
+app.get('/solutions', (req, res) => {
+    res.sendFile( path.join(publicDir, 'solutions.html') );
+});
 app.listen(port, () => {
     console.log('App running on ' + port);
 });
